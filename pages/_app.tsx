4c89a84ff44b385c3debb2aed7291b89a3a63103
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-15 18:56:13 -0300

    agregado linter y prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp ({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
 
