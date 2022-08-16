b5c606ba5091c5df9905de6d3674917424986a3d
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-15 22:55:24 -0300

    add: getAllProducts and createProduct

---
+++ server/controllers/products/products.controller.ts
@@ -0,0 +1,78 @@
+/* eslint-disable no-useless-catch */
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+const getProducts = async () => {
+  try {
+    const products = await prisma.products.findMany()
+    return products
+  } catch (error) {
+    throw error
+  }
+}
+
+const createProduct = async (name: string, price: number) => {
+  try {
+    const productCreated = await prisma.products.create({
+      data: { name, price }
+    })
+
+    return productCreated
+  } catch (error) {
+    throw error
+  }
+}
+
+const getProductById = async (id: string) => {
+  try {
+    const product = await prisma.products.findUniqueOrThrow({
+      where: {
+        id
+      }
+    })
+    return product
+  } catch (error) {
+    throw error
+  }
+}
+
+const updateProductById = async (id: string, name: string, price: number) => {
+  try {
+    await prisma.products.findUniqueOrThrow({ where: { id } })
+
+    if (!name || !price) {
+      throw new Error('Name or price must be provided!')
+    }
+
+    const updatedUser = await prisma.products.update({
+      where: { id },
+      data: {
+        name,
+        price
+      }
+    })
+
+    return updatedUser
+  } catch (error) {
+    throw error
+  }
+}
+
+const deleteUserById = async (id: string) => {
+  try {
+    const deletedUser = await prisma.products.delete({
+      where: { id }
+    })
+    return deletedUser
+  } catch (error) {
+    throw error
+  }
+}
+
+export {
+  getProducts,
+  createProduct,
+  getProductById,
+  updateProductById,
+  deleteUserById
+}
