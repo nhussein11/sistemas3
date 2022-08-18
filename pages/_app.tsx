0154da0106aac17249efd5451a8c64de32e5dbce
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-18 21:42:08 +0000

    added recoil state management

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,16 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
-
+import { RecoilRoot } from 'recoil'
 function MyApp ({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient())
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </RecoilRoot>
   )
 }
 
