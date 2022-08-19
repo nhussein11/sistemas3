d6117d713622007d904db3cfea0e22a010ad879d
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-19 20:38:50 +0000

    fix: axios baseurl + /api to prod url

--- src/frontend/axios-api/axios-api.ts
+++ src/frontend/axios-api/axios-api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosRequestConfig } from 'axios'
 import { EnvService } from '../../shared/services/env.service'
 
 const axiosConfig: AxiosRequestConfig = {
-  baseURL: (EnvService.isProd()) ? 'https://sistemas3.vercel.app/' : 'http://localhost:3000/api',
+  baseURL: (EnvService.isProd()) ? 'https://sistemas3.vercel.app/api' : 'http://localhost:3000/api',
   headers: {
     'Content-type': 'application/json',
     Accept: 'application/json'
