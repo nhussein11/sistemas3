a12a6a4bb1b00cdc0752dba3fd12181dcc245dbf
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-18 23:21:56 -0300

    add: replace for envservice logic

--- src/backend/server/prisma-client/prisma-client.ts
+++ src/backend/server/prisma-client/prisma-client.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client'
+import { EnvService } from '../../../shared/services/env.service'
 declare global {
   // allow global `var` declarations
   // eslint-disable-next-line no-var, no-unused-vars
@@ -7,4 +8,4 @@ declare global {
 
 export const prisma = global.prisma || new PrismaClient({})
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma
+if (EnvService.isProd()) global.prisma = prisma
