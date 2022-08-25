dbc98f292f75c40e62419e3179bb5a70ed039c0f
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-25 14:11:47 +0000

    added useField Hook
    manages state,changes and type of input elements

--- src/frontend/hooks/useField.tsx
+++ src/frontend/hooks/useField.tsx
@@ -1,10 +1,36 @@
 import React, { useState } from 'react'
+type FieldEvent =
+  | React.ChangeEvent<HTMLInputElement>
+  | string
+  | number
+  | undefined
+type FieldProps = {
+  initialValue: string | number | undefined
+  handleChange?: (e: FieldEvent) => void | undefined
+  type: string
+}
 
-const useField = ( initialValue, type, handleChange=null ) => {
+const useField = ({
+  initialValue,
+  type,
+  handleChange = undefined
+}: FieldProps) => {
   const [value, setValue] = useState(initialValue)
-  const onChange = (e) => {
-    onChange(e.target.value)
-    handleChange?.(e.target.value)
+  const onChange = (e: FieldEvent) => {
+    if (typeof e === 'string' || typeof e === 'undefined') {
+      setValue(e)
+      return
+    }
+    if (typeof e === 'number') {
+      setValue(Number(e).valueOf())
+      return
+    }
+    if (typeof e === 'object') {
+      setValue(
+        type === 'number' ? Number(e.target.value).valueOf() : e.target.value
+      )
+      handleChange?.(e.target.value)
+    }
   }
   return { value, onChange, type }
 }
