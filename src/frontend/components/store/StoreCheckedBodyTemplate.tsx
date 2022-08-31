d1d64033056f38d0e0961420a856bd4391152e71
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-08-31 23:34:25 +0000

    added checking functionality on stores table

--- src/frontend/components/store/StoreCheckedBodyTemplate.tsx
+++ src/frontend/components/store/StoreCheckedBodyTemplate.tsx
@@ -1,18 +1,31 @@
 import { Checkbox } from 'primereact/checkbox'
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import { isStoreCheckedState } from '../../atoms/isStoreCheckedAtom'
+import { defaultStore, selectedStoreState } from '../../atoms/selectedStoreAtom'
 
 const StoreCheckedBodyTemplate = ({ rowData }) => {
+  const [, setSelectedStore] = useRecoilState(selectedStoreState)
+  const [isStoreChecked, setIsStoreChecked] =
+    useRecoilState(isStoreCheckedState)
   // brind selected store from recoil state
   // brind is selected store from recoil state
   const handleCheck = () => {
-    // set selected store
-    // set is selected store
+    if (isStoreChecked.id === rowData.id) {
+      setIsStoreChecked({ id: '', checked: false })
+      setSelectedStore(defaultStore)
+      return
+    }
+    setIsStoreChecked({ id: rowData.id, checked: true })
+    setSelectedStore(rowData)
   }
+  // set selected store
+  // set is selected store
   return (
     <Checkbox
       onChange={() => handleCheck()}
       value={rowData.id}
-      checked={false} // change this with comparison of selected store and row data
+      checked={isStoreChecked.id === rowData.id} // change this with comparison of selected store and row data
     ></Checkbox>
   )
 }
