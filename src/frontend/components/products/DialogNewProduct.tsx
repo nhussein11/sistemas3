307c004f7d849a9e09e0126101b1f53cd8f57577
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-01 02:48:38 +0000

    added select store functionality on create product

--- src/frontend/components/products/DialogNewProduct.tsx
+++ src/frontend/components/products/DialogNewProduct.tsx
@@ -1,21 +1,30 @@
 import { Dialog } from 'primereact/dialog'
 import { InputText } from 'primereact/inputtext'
 import { Dropdown } from 'primereact/dropdown'
-import React, { useState } from 'react'
+import React from 'react'
 import { DialogNewProductProps } from '../../@types/frontend.types'
 import useDialogNewProductMutation from '../../hooks/products/useDialogNewProductMutation'
-import { CategoryEnum } from '@prisma/client'
-
 import DialogFooter from './DialogFooter'
+import { Store } from '../../../shared/schemas/store.type'
 
 const DialogNewProduct = ({
   displayBasic,
   closeDialog
 }: DialogNewProductProps) => {
-  const { handleCreateNewProduct, productName, productPrice, productDescription, productCategory } =
-    useDialogNewProductMutation('products')
-  const [category, setCategory] = useState('IMPRESORA')
-  const CATEGORIES = [CategoryEnum.IMPRESORA, CategoryEnum.FILAMENTO]
+  const {
+    handleCreateNewProduct,
+    productName,
+    productPrice,
+    productDescription,
+    category,
+    setCategory,
+    CATEGORIES,
+    storesQuery,
+    changeStore,
+    selectedStore,
+    productQuantity,
+    productMinQuantity
+  } = useDialogNewProductMutation('products')
   return (
     <Dialog
       visible={displayBasic}
@@ -24,20 +33,37 @@ const DialogNewProduct = ({
       footer={() => DialogFooter({ closeDialog, handleCreateNewProduct })}
       onHide={() => closeDialog()}
     >
-      <div className='form-container'>
+      <div className="form-container">
         <div className="field-form-container">
           <span className="p-float-label">
             <InputText {...productName} name="productName" />
             <label htmlFor="in">Nombre</label>
           </span>
-          <Dropdown value={productCategory} options={CATEGORIES} onChange={(e) => setCategory(e.value)} placeholder={category}/>
+          <Dropdown
+            value={category}
+            options={CATEGORIES}
+            onChange={(e) => {
+              setCategory(e.value)
+            }}
+            placeholder={category}
+          />
+          <Dropdown
+            value={selectedStore.name}
+            options={storesQuery?.data?.stores.map(
+              (store: Store) => store.name
+            )}
+            onChange={(e) => changeStore(e.target.value)}
+            placeholder="select a Store"
+          />
         </div>
-        <div className='field-form-container'>
+        <div className="field-form-container">
           <span className="p-float-label">
             <InputText {...productDescription} name="productDescription" />
             <label htmlFor="in">Descripción</label>
           </span>
           <InputText {...productPrice} name="productPrice" />
+          <InputText {...productQuantity} name="productQuantity" />
+          <InputText {...productMinQuantity} name="productMinQuantity" />
         </div>
       </div>
     </Dialog>
