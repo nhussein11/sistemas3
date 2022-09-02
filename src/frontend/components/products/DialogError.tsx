2acbe5856dfcc233d8a51c2d6d9e532b60648c1f
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-02 03:39:12 +0000

    added listening to global state on DialogError

--- src/frontend/components/products/DialogError.tsx
+++ src/frontend/components/products/DialogError.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import { Dialog } from 'primereact/dialog'
 import useDialogUpdateProductMutation from '../../hooks/products/useDialogUpdateProductMutation'
+import { useRecoilState } from 'recoil'
+import { defaultErrorState, ErrorState } from '../../atoms/ErrorAtom'
 
 const DialogError = () => {
-  const {
-    showErrorDialog,
-    setShowErrorDialog
-  } = useDialogUpdateProductMutation('products')
+  const [errorState, setErrorState] = useRecoilState(ErrorState)
+  const { showErrorDialog, setShowErrorDialog } =
+    useDialogUpdateProductMutation('products')
 
   return (
     <Dialog
       visible={showErrorDialog}
       header="Error"
       style={{ width: '50vw' }}
-      onHide={() => setShowErrorDialog(false)}
+      onHide={() => {
+        setShowErrorDialog(false)
+        setErrorState(defaultErrorState)
+      }}
     >
       <div className="field-form-container">
-        <span className="p-float-label" >
-            Id not provided!
-        </span>
+        <span className="p-float-label">{errorState.message}</span>
       </div>
     </Dialog>
   )
