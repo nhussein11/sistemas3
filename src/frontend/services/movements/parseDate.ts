2901265bc32014d3db26f77a06757c003edf9665
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-08 21:42:06 +0000

    removed clog

--- src/frontend/services/movements/parseDate.ts
+++ src/frontend/services/movements/parseDate.ts
@@ -1,5 +1,4 @@
 export const parseDate = (date: string): string => {
-  console.log(date)
   const [year, month, day] = date.split('-')
   const parsedDay = day.split('T')[0]
   return `${parsedDay}/${month}/${year}`
