452a5b0271317e5c57f71ba651e297f7c4920f54
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-08 22:02:13 +0000

    changed filter method

--- src/frontend/services/movements/filterMovements.ts
+++ src/frontend/services/movements/filterMovements.ts
@@ -2,6 +2,6 @@ import { Movement } from '@prisma/client'
 
 export const filterMovements = (movements: Movement[], search: string) => {
   return movements?.filter((movement) => {
-    return movement.movementTypeId.toLowerCase().includes(search.toLowerCase())
+    return movement.observation.toLowerCase().includes(search.toLowerCase())
   })
 }
