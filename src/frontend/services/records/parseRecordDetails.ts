a61db2ccf3e0d3b44ec3a6cada072379dad28dbb
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-13 22:27:08 +0000

    refactored services

--- src/frontend/services/records/parseRecordDetails.ts
+++ src/frontend/services/records/parseRecordDetails.ts
@@ -1,10 +1,19 @@
+import { Product } from '@prisma/client'
+import { UseQueryResult } from '@tanstack/react-query'
 import { RecordDetailData } from '../../@types/frontend.types'
 
-export const ParseRecordDetails = (records: RecordDetailData[]) => {
-  return records.map((record: RecordDetailData) => {
+export const ParseRecordDetails = (
+  records: RecordDetailData[],
+  productsQuery: UseQueryResult<{ products: Product[] }, unknown>
+) => {
+  return records.map((record) => {
+    const productPrice = productsQuery?.data?.products?.find(
+      (product: Product) => product.id === record.productId
+    )?.price
     return {
-      productId: record.productId,
-      quantity: record.quantity
+      stockId: record.stockId,
+      quantity: record.quantity,
+      subTotal: productPrice ? productPrice * record.quantity : 0
     }
   })
 }
