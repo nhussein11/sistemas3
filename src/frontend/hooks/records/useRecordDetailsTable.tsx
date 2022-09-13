b0e2e7b4a13c5fbe9902e23c9f64e230f494c734
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-13 22:55:32 +0000

    added displaying record details

--- src/frontend/hooks/records/useRecordDetailsTable.tsx
+++ src/frontend/hooks/records/useRecordDetailsTable.tsx
@@ -1,16 +1,37 @@
-import { RecordDetails } from '@prisma/client'
+import { RecordDetails, Stock } from '@prisma/client'
 import { useRecoilState } from 'recoil'
 import { selectedRecordState } from '../../atoms/records/selectedRecordAtom'
+import { findProductName } from '../../services/products/findProductName'
+import { findStoreName } from '../../services/stores/findStoreName'
 import useDetailsQuery from '../details/useDetailsQuery'
+import useProductsQuery from '../products/useProductsQuery'
+import useStocksQuery from '../stock/useStocksQuery'
+import useStoresQuery from '../stores/useStoresQuery'
 
 const useRecordDetailsTable = () => {
   const detailsQuery = useDetailsQuery('details')
+  const stocksQuery = useStocksQuery('stocks')
+  const productsQuery = useProductsQuery('products')
+  const storesQuery = useStoresQuery('stores')
   const [selectedRecord] = useRecoilState(selectedRecordState)
   const filteredDetails: RecordDetails[] =
     detailsQuery.data?.recordsDetails?.filter(
       (d: RecordDetails) => d.recordId === selectedRecord.id
     )
-  return { filteredDetails }
+  const filteredDetailsTableData = filteredDetails?.map((detail) => {
+    const stock = stocksQuery.data?.stocks.find(
+      (stock: Stock) => stock.id === detail.stockId
+    )
+    const productId = stock?.productId
+    const storeId = stock?.storeId
+    return {
+      stockId: detail.stockId,
+      productName: findProductName(productId, productsQuery),
+      storeName: findStoreName(storeId, storesQuery),
+      quantity: detail.quantity
+    }
+  })
+  return { filteredDetailsTableData }
 }
 
 export default useRecordDetailsTable
