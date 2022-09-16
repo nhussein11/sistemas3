e06b904b4baf1cdf31171fc18e6f7c7270b4bfb2
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-09-16 15:29:34 +0000

    Added: option column

--- src/frontend/components/store/StoresTable.tsx
+++ src/frontend/components/store/StoresTable.tsx
@@ -1,15 +1,23 @@
 import React, { useState } from 'react'
 import { Column } from 'primereact/column'
+import { Button } from 'primereact/button'
 import { DataTable } from 'primereact/datatable'
 import DialogError from '../products/DialogError'
 import DialogNewStore from './DialogNewStore'
+import { showUpdateDialogState } from '../../atoms/showUpdateDialogAtom'
 import DialogUpdateStore from './DialogUpdateStore'
 import { StoresTableProps } from '../../@types/frontend.types'
-import StoreCheckedBodyTemplate from './StoreCheckedBodyTemplate'
 import StoreTableHeader from './StoreTableHeader'
+import { useRecoilState } from 'recoil'
+import { selectedStoreState } from '../../atoms/stores/selectedStoreAtom'
+import useDeleteStoreMutation from '../../hooks/stores/useDeleteStoreMutation'
 
 const StoresTable = ({ stores }:StoresTableProps) => {
   const [displayBasic, setDisplayBasic] = useState(false)
+  const [, setShowUpdateDialog] = useRecoilState(showUpdateDialogState)
+  const [, setSelectedStore] = useRecoilState(selectedStoreState)
+  const { handleDeleteStore } = useDeleteStoreMutation('stores')
+
   return (
     <div className="datatable-filter">
       <div className="card">
@@ -25,16 +33,6 @@ const StoresTable = ({ stores }:StoresTableProps) => {
           emptyMessage="No se encontraron stores"
         >
           <Column
-            field="select"
-            header="Select"
-            body={(rowData) =>
-              StoreCheckedBodyTemplate({
-                rowData
-              })
-            }
-            alignHeader={'center'}
-          />
-          <Column
             field="Nombre"
             header="Nombre"
             body={(rowData) => rowData.name}
@@ -44,6 +42,39 @@ const StoresTable = ({ stores }:StoresTableProps) => {
             field="Direccion"
             header="Direccion"
             body={(rowData) => rowData.address}
+            alignHeader={'center'}
+          />
+          <Column
+            field="options"
+            header="Opciones"
+            style={{ minWidth: '1rem' }}
+            body={(rowData) => {
+              return (
+                <div>
+                  <Button
+                  icon="pi pi-pencil"
+                  iconPos="right"
+                  label="Editar"
+                  className="p-button-p-button-raised p-button-warning"
+                  onClick={() => {
+                    setSelectedStore(rowData)
+                    setShowUpdateDialog(true)
+                  }}
+                  />
+                  <Button
+                  icon="pi pi-trash"
+                  iconPos="right"
+                  label="Borrar"
+                  className="p-button-p-button-raised p-button-danger"
+                  onClick={() => {
+                    setSelectedStore(rowData)
+                    handleDeleteStore()
+                    setDisplayBasic(false)
+                  }}
+                  />
+                </div>
+              )
+            }}
             alignHeader={'center'}
           />
         </DataTable>
