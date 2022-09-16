14ea5826f9ef741cea25376ace11b1a2d53e490c
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-09-16 14:37:35 +0000

    changed: table with options, deleted: select column

--- src/frontend/components/stock/StockTableHeader.tsx
+++ src/frontend/components/stock/StockTableHeader.tsx
@@ -1,11 +1,7 @@
 import React from 'react'
-import { Button } from 'primereact/button'
 import { InputText } from 'primereact/inputtext'
-import { isStockCheckedState } from '../../atoms/stock/isStockSelectedAtom'
 import { globalFilterValueState } from '../../atoms/globalFilterValueAtom'
 import { StockTableHeaderProps } from '../../@types/frontend.types'
-import { showErrorDialogState } from '../../atoms/error/showErrorDialog'
-import { showUpdateDialogState } from '../../atoms/showUpdateDialogAtom'
 // import useDeleteStockMutation from '../../hooks/stock/useDeleteStockMutation'
 import { useRecoilState } from 'recoil'
 
@@ -13,15 +9,7 @@ const StockTableHeader = ({ setDisplayBasic }:StockTableHeaderProps) => {
   const [globalFilterValue, setGlobalFilterValue] = useRecoilState(
     globalFilterValueState
   )
-  const [isStockChecked] = useRecoilState(isStockCheckedState)
-  const [, setShowUpdateDialog] = useRecoilState(showUpdateDialogState)
-  const [, setShowErrorDialog] = useRecoilState(showErrorDialogState)
   // const { handleDeleteStock } = useDeleteStockMutation('stocks')
-  const updateStock = () => {
-    isStockChecked.checked
-      ? setShowUpdateDialog(true)
-      : setShowErrorDialog(true)
-  }
   return (
     <div className="header-table">
       <div className="flex justify-content-between">
@@ -33,13 +21,6 @@ const StockTableHeader = ({ setDisplayBasic }:StockTableHeaderProps) => {
             placeholder="Buscar"
           />
         </span>
-      </div>
-      <div className="actionsButtonsTable">
-        <Button
-          label="Modificar"
-          className="p-button-raised p-button-secondary"
-          onClick={updateStock}
-        />
       </div>
     </div>
   )
