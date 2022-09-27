c01f69fd9c575397a4a37cdce0ec9ce766142f8d
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-27 18:09:38 +0000

    corrected endpoint

--- src/frontend/services/courses/deleteCourse.ts
+++ src/frontend/services/courses/deleteCourse.ts
@@ -1,7 +1,7 @@
 import publicAxiosInstance from '../../api/axios-api'
 export const deleteCourse = async (courseId: string) => {
   const response = await publicAxiosInstance.delete(
-    `/course/${courseId}`,
+    `/courses/${courseId}`,
     {}
   )
   return response
