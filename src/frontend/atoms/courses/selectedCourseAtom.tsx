995454b7e2034f405b41a6e4071d9fc4dac72736
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-27 18:35:14 +0000

    refactored course  atom

--- src/frontend/atoms/courses/selectedCourseAtom.tsx
+++ src/frontend/atoms/courses/selectedCourseAtom.tsx
@@ -5,7 +5,8 @@ export const defaultCourse:CourseFrontend = {
   id: '',
   name: '',
   description: '',
-  price: 0
+  price: 0,
+  hoursQuantity: 0
 }
 export const selectedCourseState = atom({
   key: 'selectedCourseState',
