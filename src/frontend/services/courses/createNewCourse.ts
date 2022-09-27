c1ffc548df1d37ba1da2fee2a6869d9aedb38429
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-09-27 18:12:11 +0000

    added hours quantity to services

--- src/frontend/services/courses/createNewCourse.ts
+++ src/frontend/services/courses/createNewCourse.ts
@@ -3,6 +3,7 @@ type CourseData = {
   name: string
   description: string
   price: number
+  hoursQuantity: number
 }
 export const createNewCourse = async (course: CourseData) => {
   const response = await publicAxiosInstance.post('/courses', course)
--- src/frontend/services/courses/updateCourse.ts
+++ src/frontend/services/courses/updateCourse.ts
@@ -1,17 +1,25 @@
 import publicAxiosInstance from '../../api/axios-api'
 
-export type CourseUpdateData={
-  id:string,
-  name?:string,
-  description?:string,
-  price?:number,
+export type CourseUpdateData = {
+  id: string
+  name?: string
+  description?: string
+  price?: number
+  hoursQuantity?: number
 }
 
-export const updateCourse = async ({ id, name, price, description }: CourseUpdateData) => {
+export const updateCourse = async ({
+  id,
+  name,
+  price,
+  description,
+  hoursQuantity
+}: CourseUpdateData) => {
   const response = await publicAxiosInstance.put(`/courses/${id}`, {
     name,
     price,
-    description
+    description,
+    hoursQuantity
   })
   return response
 }
