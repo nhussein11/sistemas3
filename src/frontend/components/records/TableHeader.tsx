8c25b1da3818e6074a3f81a8ac0b0f6ea2a0a754
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-04 01:47:33 +0000

    refactor tags

--- src/frontend/components/records/TableHeader.tsx
+++ src/frontend/components/records/TableHeader.tsx
@@ -5,32 +5,19 @@ import { useRecoilState } from 'recoil'
 import { RecordsTableHeaderProps } from '../../@types/frontend.types'
 import { globalFilterValueState } from '../../atoms/globalFilterValueAtom'
 
-const TableHeader = ({
-  setDisplayBasic,
-  setDisplayRecordDetailsTable
-}: RecordsTableHeaderProps) => {
-  const [globalFilterValue, setGlobalFilterValue] = useRecoilState(
-    globalFilterValueState
-  )
+const TableHeader = ({ setDisplayBasic, setDisplayRecordDetailsTable }: RecordsTableHeaderProps) => {
+  const [globalFilterValue, setGlobalFilterValue] = useRecoilState(globalFilterValueState)
 
   return (
     <div className="header-table">
       <div className="flex justify-content-between">
         <span className="p-input-icon-left">
           <i className="pi pi-search" />
-          <InputText
-            value={globalFilterValue}
-            onChange={(e) => setGlobalFilterValue(e.target.value)}
-            placeholder="Buscar"
-          />
+          <InputText value={globalFilterValue} onChange={(e) => setGlobalFilterValue(e.target.value)} placeholder="Buscar"/>
         </span>
       </div>
       <div className="actionsButtonsTable">
-        <Button
-          label="Nuevo"
-          className="p-button-raised p-button-success"
-          onClick={() => setDisplayBasic(true)}
-        />
+        <Button label="Nuevo" className="p-button-raised p-button-success" onClick={() => setDisplayBasic(true)}/>
       </div>
     </div>
   )
