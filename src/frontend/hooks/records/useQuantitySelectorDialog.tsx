5e6d6b05ca8e2e6fd39b63b352b6ee167f5554a5
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-04 02:10:11 +0000

    restructure tags

--- src/frontend/hooks/records/useQuantitySelectorDialog.tsx
+++ src/frontend/hooks/records/useQuantitySelectorDialog.tsx
@@ -1,21 +1,12 @@
 import { useRecoilState } from 'recoil'
 import { selectedRecordDetailsState } from '../../atoms/records/selectedRecordDetails'
-import {
-  showQuantitySelectorDialogDefaultState,
-  showQuantitySelectorDialogState
-} from '../../atoms/records/showQuantitySelectorDialog'
+import { showQuantitySelectorDialogDefaultState, showQuantitySelectorDialogState } from '../../atoms/records/showQuantitySelectorDialog'
 import useField from '../useField'
 
 const useQuantitySelectorDialog = () => {
-  const [showQuantitySelectorDialog, setShowQuantitySelectorDialog] =
-    useRecoilState(showQuantitySelectorDialogState)
-  const quantity = useField({
-    type: 'number',
-    initialValue: 0
-  })
-  const [, setSelectedRecordDetails] = useRecoilState(
-    selectedRecordDetailsState
-  )
+  const [showQuantitySelectorDialog, setShowQuantitySelectorDialog] = useRecoilState(showQuantitySelectorDialogState)
+  const quantity = useField({ type: 'number', initialValue: 0 })
+  const [, setSelectedRecordDetails] = useRecoilState(selectedRecordDetailsState)
   const addDetail = () => {
     setSelectedRecordDetails((prev) => [
       ...prev,
