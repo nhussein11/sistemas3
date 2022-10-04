67e48f31ac34332447ba8649392b2db70a44dba4
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-04 18:20:22 +0000

    add new props

--- src/frontend/components/records/newRecord/SaleDataBar.tsx
+++ src/frontend/components/records/newRecord/SaleDataBar.tsx
@@ -4,8 +4,8 @@ import { Dropdown } from 'primereact/dropdown'
 import { InputText } from 'primereact/inputtext'
 import { Customer, Supplier } from '@prisma/client'
 
-const SaleDataBar = ({ customers, suppliers, selectedCustomer, selectedSupplier }:
-  {customers: Object[]; suppliers: Object[]; selectedCustomer: Customer; selectedSupplier: Supplier }) => {
+const SaleDataBar = ({ customers, suppliers, selectedCustomer, selectedSupplier, changeCustomer, changeSupplier }:
+  {customers: Object[]; suppliers: Object[]; selectedCustomer: Customer; selectedSupplier: Supplier; changeCustomer: any; changeSupplier: any }) => {
   return (
     <div style={{ display: 'flex', justifyContent: 'space-around' }}>
         <span className="p-float-label">
@@ -24,10 +24,10 @@ const SaleDataBar = ({ customers, suppliers, selectedCustomer, selectedSupplier 
           <Dropdown options={['A', 'B', 'C']} placeholder="Tipo Factura"/>
         </div>
         <div className="field">
-          <Dropdown options={customers} value={selectedCustomer?.name} placeholder="Seleccionar Cliente"/>
+          <Dropdown options={customers} value={selectedCustomer?.name} onChange={(e) => changeCustomer(e.target.value)} placeholder="Seleccionar Cliente"/>
         </div>
         <div className="field">
-            <Dropdown options={suppliers} value={selectedSupplier?.name} placeholder="Seleccionar Proveedor"/>
+            <Dropdown options={suppliers} value={selectedSupplier?.name} onChange={(e) => changeSupplier(e.target.value)} placeholder="Seleccionar Proveedor"/>
         </div>
     </div>
   )
