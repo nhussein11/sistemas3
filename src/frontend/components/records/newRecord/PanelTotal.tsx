e537cd203beb521ac00ed04ace24f14a771f24b1
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-05 01:56:41 +0000

    add format number

--- src/frontend/components/records/newRecord/PanelTotal.tsx
+++ src/frontend/components/records/newRecord/PanelTotal.tsx
@@ -3,16 +3,21 @@ import React from 'react'
 import { Panel } from 'primereact/panel'
 import { Card } from 'primereact/card'
 import { Button } from 'primereact/button'
+import { defaultAmmount, ammountRecordAtomState } from '../../../../frontend/atoms/records/ammountRecordAtom'
+import { useRecoilState } from 'recoil'
+import NumberFormat from 'react-number-format'
 
-const PanelTotal = ({ handleCreateNewRecord }: {handleCreateNewRecord: any}) => {
+const PanelTotal = ({ handleCreateNewRecord }: {handleCreateNewRecord: any;}) => {
+  const [ammount, setAmmount] = useRecoilState(ammountRecordAtomState)
   return (
     <div className='container-total'>
         <Panel>
             <Card>
-                <h1>TOTAL: $10.000</h1>
+                <h1><NumberFormat value={ammount.ammount} displayType={'text'} thousandSeparator={'.'} decimalSeparator={','} prefix={'$'}></NumberFormat></h1>
             </Card>
             <Button style={{ width: '-webkit-fill-available', justifyContent: 'center' }} onClick={() => {
               handleCreateNewRecord()
+              setAmmount(defaultAmmount)
             }} className="p-button-success"><h2>GENERAR COMPROBANTE</h2></Button>
         </Panel>
     </div>
