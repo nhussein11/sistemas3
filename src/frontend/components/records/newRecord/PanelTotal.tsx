545a102802b1fa99c6203c39577eb5656bfb4916
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-05 01:03:48 +0000

    fix details

--- src/frontend/components/records/newRecord/PanelTotal.tsx
+++ src/frontend/components/records/newRecord/PanelTotal.tsx
@@ -2,7 +2,6 @@
 import React from 'react'
 import { Panel } from 'primereact/panel'
 import { Card } from 'primereact/card'
-import { InputNumber } from 'primereact/inputnumber'
 import { Button } from 'primereact/button'
 
 const PanelTotal = ({ handleCreateNewRecord }: {handleCreateNewRecord: any}) => {
@@ -12,12 +11,9 @@ const PanelTotal = ({ handleCreateNewRecord }: {handleCreateNewRecord: any}) => 
             <Card>
                 <h1>TOTAL: $10.000</h1>
             </Card>
-            <div style={{ display: 'grid', gridTemplateColumns: 'auto auto', margin: '1rem', columnGap: '3rem' }}>
-                <InputNumber id='paid' placeholder='Ingresar Importe' mode="currency" currency="USD" locale="en-US" />
-            </div>
             <Button style={{ width: '-webkit-fill-available', justifyContent: 'center' }} onClick={() => {
               handleCreateNewRecord()
-            }} className="p-button-success"><h2>GENERAR VENTA</h2></Button>
+            }} className="p-button-success"><h2>GENERAR COMPROBANTE</h2></Button>
         </Panel>
     </div>
   )
