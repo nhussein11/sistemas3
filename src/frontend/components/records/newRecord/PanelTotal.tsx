bf916c76d87cc21390a387af61336a6b2521721e
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-05 03:51:36 +0000

    add labels obs and adr

--- src/frontend/components/records/newRecord/PanelTotal.tsx
+++ src/frontend/components/records/newRecord/PanelTotal.tsx
@@ -6,14 +6,25 @@ import { Button } from 'primereact/button'
 import { defaultAmmount, ammountRecordAtomState } from '../../../../frontend/atoms/records/ammountRecordAtom'
 import { useRecoilState } from 'recoil'
 import NumberFormat from 'react-number-format'
+import { InputText } from 'primereact/inputtext'
 
-const PanelTotal = ({ handleCreateNewRecord }: {handleCreateNewRecord: any;}) => {
+const PanelTotal = ({ handleCreateNewRecord, recordObservation, recordAdress }: {handleCreateNewRecord: any; recordObservation: any; recordAdress: any}) => {
   const [ammount, setAmmount] = useRecoilState(ammountRecordAtomState)
   return (
     <div className='container-total'>
         <Panel>
             <Card>
-                <h1><NumberFormat value={ammount.ammount} displayType={'text'} thousandSeparator={'.'} decimalSeparator={','} prefix={'$'}></NumberFormat></h1>
+            <span className="p-float-label">
+                <InputText style={{ width: '100%' }} {...recordObservation} id="obeservation" />
+                <label htmlFor="obeservation">Observación</label>
+            </span>
+            <span className="p-float-label" style={{ marginTop: '2rem' }}>
+                <InputText style={{ width: '100%' }} {...recordAdress} id="adress" />
+                <label htmlFor="adress">Dirección</label>
+            </span>
+            </Card>
+            <Card>
+                <h1>TOTAL: <NumberFormat value={ammount.ammount} displayType={'text'} thousandSeparator={'.'} decimalSeparator={','} prefix={'$'}></NumberFormat></h1>
             </Card>
             <Button style={{ width: '-webkit-fill-available', justifyContent: 'center' }} onClick={() => {
               handleCreateNewRecord()
