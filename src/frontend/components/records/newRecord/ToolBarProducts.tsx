94a918ace2072b130b43359a3d2d7939f3096b3c
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-05 03:53:17 +0000

    add function to set search name

--- src/frontend/components/records/newRecord/ToolBarProducts.tsx
+++ src/frontend/components/records/newRecord/ToolBarProducts.tsx
@@ -2,22 +2,33 @@ import React from 'react'
 import { Toolbar } from 'primereact/toolbar'
 import { Button } from 'primereact/button'
 import { InputText } from 'primereact/inputtext'
+import { RecordNameEnum } from '@prisma/client'
 
 // eslint-disable-next-line react/prop-types
-export default function ToolBarProducts ({ idRef, addProductBarCode, setVisibleTableProducts }) {
+export default function ToolBarProducts ({ setVisibleTableProducts, recordName }: { setVisibleTableProducts: any; recordName: string }) {
   const leftToolbarTemplate = () => {
     return (
     <React.Fragment>
-        <Button label="Productos" icon="pi pi-search" className="p-button-success mr-2" onClick={() => setVisibleTableProducts()} />
+        <Button label={getSearchName()} icon="pi pi-search" className="p-button-success mr-2" onClick={() => setVisibleTableProducts()} />
     </React.Fragment>
     )
+  }
+  function getSearchName () {
+    switch (recordName) {
+      case RecordNameEnum.FACTURA_DUPLICADO:
+      case RecordNameEnum.FACTURA_ORIGINAL:
+        return 'Productos'
+      case RecordNameEnum.ORDEN_DE_COMPRA:
+      case RecordNameEnum.ORDEN_DE_PAGO:
+        return 'Facturas'
+    }
   }
 
   const rightToolbarTemplate = () => {
     return (
     <span className="p-input-icon-left">
         <i className="pi pi-plus" />
-        <InputText ref={idRef} type="search" placeholder="Codigo Barras" onKeyDown={addProductBarCode} autoFocus/>
+        <InputText type="search" placeholder="Codigo Barras" autoFocus/>
     </span>
     )
   }
