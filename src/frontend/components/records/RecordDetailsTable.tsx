0d2e1885f49e0cadd6dac406ba9cef24443a39fd
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-05 15:19:55 +0000

    restructure tags

--- src/frontend/components/records/RecordDetailsTable.tsx
+++ src/frontend/components/records/RecordDetailsTable.tsx
@@ -4,48 +4,17 @@ import { Dialog } from 'primereact/dialog'
 import React from 'react'
 import { DialogRecordDetailsProps } from '../../@types/frontend.types'
 import useRecordDetailsTable from '../../hooks/records/useRecordDetailsTable'
-const RecordDetailsTable = ({
-  setDisplayRecordDetailsTable,
-  displayRecordDetailsTable
-}: DialogRecordDetailsProps) => {
+const RecordDetailsTable = ({ setDisplayRecordDetailsTable, displayRecordDetailsTable }: DialogRecordDetailsProps) => {
   const { filteredDetailsTableData } = useRecordDetailsTable()
-  console.log(filteredDetailsTableData)
   if (displayRecordDetailsTable) {
     return (
-      <Dialog
-        header="Detalles del Movimiento"
-        visible={displayRecordDetailsTable}
-        style={{ width: '50vw' }}
-        onHide={() => setDisplayRecordDetailsTable(false)}
-      >
-        <DataTable
-          value={filteredDetailsTableData}
-          paginator
-          className="p-datatable-customers"
-          showGridlines
-          rows={10}
-          dataKey="id"
-          responsiveLayout="scroll"
-          emptyMessage="No se encontraron Detalles"
-        >
-          <Column
-            field="Product"
-            header="Producto"
-            body={(rowData) => rowData.productName}
-            alignHeader={'center'}
-          />
-          <Column
-            field="Deposito"
-            header="Deposito"
-            body={(rowData) => rowData.storeName}
-            alignHeader={'center'}
-          />
-          <Column
-            field="Quantity"
-            header="Cantidad"
-            body={(rowData) => rowData.quantity}
-            alignHeader={'center'}
-          />
+      <Dialog header="Detalles del Movimiento" visible={displayRecordDetailsTable} style={{ width: '50vw' }} onHide={() => setDisplayRecordDetailsTable(false)}>
+        <DataTable value={filteredDetailsTableData} paginator className="p-datatable-customers" showGridlines rows={5} dataKey="id" responsiveLayout="scroll" emptyMessage="No se encontraron Detalles">
+          <Column field="Product" header="Producto" body={(rowData) => rowData.productName} alignHeader={'center'} />
+          <Column field="Deposito" header="Deposito" body={(rowData) => rowData.storeName} alignHeader={'center'} />
+          <Column field="Quantity" header="Cantidad" body={(rowData) => rowData.quantity} alignHeader={'center'} />
+          <Column field="Price" header="Precio" body={(rowData) => rowData.price} alignHeader={'center'} />
+          <Column field="Subtotal" header="Subtotal" body={(rowData) => rowData.subtotal} alignHeader={'center'} />
         </DataTable>
       </Dialog>
     )
