80f1edd021ba74ac0be160639fa3c2533e1d70b8
gonzaloguaimas <gonzaloguaimas@gmail.com>
2022-10-05 04:34:38 +0000

    fix problems

--- src/frontend/components/records/newRecord/TableAddedRecords.tsx
+++ src/frontend/components/records/newRecord/TableAddedRecords.tsx
@@ -4,8 +4,10 @@ import { Column } from 'primereact/column'
 import { Button } from 'primereact/button'
 import NumberFormat from 'react-number-format'
 import { Product } from '@prisma/client'
+import { resolveRecordCustomerName } from '../../../services/records/resolveRecordCustomerName'
+import { resolveRecordSupplierName } from '../../../services/records/resolveRecordSupplierName'
 
-const TableAddedProducts = ({ records, productsQuery, storesQuery }: {records: Object[]; productsQuery: any; storesQuery: any }) => {
+const TableAddedRecords = ({ records, customerQuery, supplierQuery }: {records: Object[]; customerQuery: any; supplierQuery: any }) => {
   const actionBodyTemplateListProducts = (rowData: Product) => {
     return (
     <React.Fragment>
@@ -18,10 +20,12 @@ const TableAddedProducts = ({ records, productsQuery, storesQuery }: {records: O
   }
   return (
     <DataTable value={records} paginator className="p-datatable-customers" showGridlines rows={3} dataKey="id" responsiveLayout="scroll" emptyMessage="No se agregó ningun producto">
-            <Column field="RecordNumber" header="Numero" body={(rowData) => rowData.recordNumber} style={{ minWidth: '2rem' }}></Column>
+           <Column field="RecordNumber" header="Numero" body={(rowData) => rowData.recordNumber} style={{ minWidth: '2rem' }}></Column>
             <Column field="Observation" header="Observación" body={(rowData) => rowData.observation} style={{ minWidth: '2rem' }}></Column>
-            <Column field="Observation" header="Observación" body={(rowData) => rowData.observation} style={{ minWidth: '2rem' }}></Column>
-            <Column field="SupplierClient" header="Cliente/Prov" body={(rowData) => rowData.supplierId} style={{ minWidth: '2rem' }}></Column>
+            <Column field="letter" header="Tipo" body={(rowData) => rowData.letter} style={{ minWidth: '2rem' }}></Column>
+            <Column field="SupplierClient" header="Cliente/Prov" body={(rowData) => (
+              rowData.supplierId != null ? resolveRecordCustomerName(rowData.supplierId, customerQuery) : resolveRecordSupplierName(rowData.customerId, supplierQuery)
+            )} style={{ minWidth: '2rem' }}></Column>
             <Column field="Ammount" header="Monto" alignHeader={'center'} body={(rowData) => {
               return (
                   <NumberFormat value={'123'} displayType={'text'} thousandSeparator={'.'} decimalSeparator={','} prefix={'$'}></NumberFormat>
@@ -32,4 +36,4 @@ const TableAddedProducts = ({ records, productsQuery, storesQuery }: {records: O
   )
 }
 
-export default TableAddedProducts
+export default TableAddedRecords
