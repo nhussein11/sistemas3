b40fb749ea222cbda33d7b5ae2318e7f6bcbcbf2
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-05 23:48:54 -0300

    fix: adding error response handler logic to enrollments endpoint

--- src/pages/api/enrollments/[id].ts
+++ src/pages/api/enrollments/[id].ts
@@ -5,6 +5,7 @@ import {
   getEnrollmentById,
   updateEnrollmentById
 } from '../../../backend/server/controllers/enrollments/enrollements.controller'
+import { errorHandler } from '../../utils/errorResponseHandler'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const {
@@ -23,7 +24,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const enrollment = await getEnrollmentById(id)
         return res.status(200).send({ enrollment })
       } catch (error) {
-        return res.status(500).send({ error })
+        return errorHandler(res, error)
       }
     case 'PUT':
       const { academicYear } = body
@@ -32,7 +33,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const updatedEnrollment = await updateEnrollmentById(id, academicYear)
         return res.status(200).send({ updatedEnrollment })
       } catch (error) {
-        return res.status(500).send({ error })
+        return errorHandler(res, error)
       }
 
     case 'DELETE':
@@ -40,24 +41,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const deletedEnrollment = await deleteEnrollmentById(id)
         return res.status(200).send({ deletedEnrollment })
       } catch (error: any) {
-        switch (error.code) {
-          case 'P2003':
-            return res.status(401).send({
-              error,
-              messageError:
-                "You cannot delete this product because it's belong to an existing stock"
-            })
-          case 'P2025':
-            return res.status(402).send({
-              error,
-              messageError: 'Id not provided'
-            })
-          default:
-            return res.status(450).send({
-              error,
-              messageError: 'Something failed'
-            })
-        }
+        return errorHandler(res, error)
       }
 
     default:
