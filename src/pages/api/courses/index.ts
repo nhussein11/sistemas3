4e09fba0c47db9bd80e940831e429959ab403847
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-05 23:47:58 -0300

    fix: adding error response handler logic to courses endpoint

--- src/pages/api/courses/index.ts
+++ src/pages/api/courses/index.ts
@@ -3,6 +3,7 @@ import {
   createCourse,
   getCourses
 } from '../../../backend/server/controllers/courses/courses.controller'
+import { errorHandler } from '../../utils/errorResponseHandler'
 
 export default async function courses (
   req: NextApiRequest,
@@ -16,16 +17,21 @@ export default async function courses (
         const courses = await getCourses()
         return res.status(200).send({ courses })
       } catch (error) {
-        return res.status(500).send({ error })
+        return errorHandler(res, error)
       }
 
     case 'POST':
       try {
         const { name, hoursQuantity, price, description } = body
-        const courseCreated = await createCourse(name, hoursQuantity, price, description)
+        const courseCreated = await createCourse(
+          name,
+          hoursQuantity,
+          price,
+          description
+        )
         return res.status(201).send({ courseCreated })
       } catch (error) {
-        return res.status(500).send({ error })
+        return errorHandler(res, error)
       }
 
     default:
