361b8f93a8a71059ec4bca67c95a2baf6aa894bb
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-05 23:53:29 -0300

    fix: adding error response handler logic to stores endpoint

--- src/pages/api/stores/[id].ts
+++ src/pages/api/stores/[id].ts
@@ -5,6 +5,7 @@ import {
   getStoreById,
   updateStoreById
 } from '../../../backend/server/controllers/stores/stores.controller'
+import { errorHandler } from '../../utils/errorResponseHandler'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const {
@@ -23,7 +24,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const store = await getStoreById(id)
         return res.status(200).send({ store })
       } catch (error) {
-        return res.status(500).send({ error })
+        return errorHandler(res, error)
       }
     case 'PUT':
       const { name, address } = body
@@ -32,7 +33,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const updatedStore = await updateStoreById(id, name, address)
         return res.status(200).send({ updatedStore })
       } catch (error) {
-        return res.status(500).send({ error })
+        return errorHandler(res, error)
       }
 
     case 'DELETE':
@@ -40,24 +41,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         const deletedStore = await deleteStoreById(id)
         return res.status(200).send({ deletedStore })
       } catch (error: any) {
-        switch (error.code) {
-          case 'P2003':
-            return res.status(401).send({
-              error,
-              messageError:
-                "You cannot delete this store because it's belong to an existing stock"
-            })
-          case 'P2025':
-            return res.status(402).send({
-              error,
-              messageError: 'Id not provided'
-            })
-          default:
-            return res.status(450).send({
-              error,
-              messageError: 'Something failed'
-            })
-        }
+        return errorHandler(res, error)
       }
 
     default:
