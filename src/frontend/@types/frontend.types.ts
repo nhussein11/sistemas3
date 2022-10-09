e1392567dd075a42010369a50688cd72299771e3
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-09 20:18:17 +0000

    changed: types

--- src/frontend/@types/frontend.types.ts
+++ src/frontend/@types/frontend.types.ts
@@ -1,4 +1,11 @@
-import { Product, Stock, Store, Record, Student, Enrollment } from '@prisma/client'
+import {
+  Product,
+  Stock,
+  Store,
+  Record,
+  Student,
+  Enrollment
+} from '@prisma/client'
 import { Dispatch } from 'react'
 export type CourseFrontend = {
   id: string
@@ -103,7 +110,10 @@ export type StockUpdateData = {
 }
 
 export type StockTableProps = { stocks: Stock[] }
-export type EnrollmentsTableProps = {enrollments: Enrollment[]}
+export type EnrollmentsTableProps = {
+  enrollments: Enrollment[]
+  isDialog?: boolean
+}
 export type StockDialogFooterProps = {
   closeDialog: () => void
   handleCreateNewStock: () => void
@@ -115,6 +125,7 @@ export type EnrollmentDialogFooterProps = {
 
 export type StockTableHeaderProps = {
   setDisplayBasic: Dispatch<boolean>
+  isDialog?: boolean
 }
 export type DialogFooterRecordProps = {
   closeDialog: () => void
