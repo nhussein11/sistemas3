dd89d621c86613d31976ef92339fab1c8b860af8
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-10-17 23:42:59 +0000

    refactor:service

--- src/frontend/services/enrollments/createNewEnrollment.ts
+++ src/frontend/services/enrollments/createNewEnrollment.ts
@@ -1,10 +1,11 @@
 import publicAxiosInstance from '../../api/axios-api'
 type EnrollmentData = {
   courseId: string
-  studentId: string
+  studentIds: string[]
   academicYear: number
 }
 export const createNewEnrollment = async (enrollment: EnrollmentData) => {
+  console.log(enrollment)
   const response = await publicAxiosInstance.post('/enrollments', enrollment)
   return response
 }
