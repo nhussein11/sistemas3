76f44c1996de44c62722771a0f229ba801ece15d
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-27 23:49:07 -0300

    add: historical price to records controller it was added to details array

--- src/backend/server/controllers/records/records.controller.ts
+++ src/backend/server/controllers/records/records.controller.ts
@@ -145,8 +145,8 @@ const handleRecordDetailsCreation = async (
 
   const promiseArrayMovementsDetails = details.map(
     (stockIdQuantityAndSubtotal: any) => {
-      const { stockId, quantity, subtotal } = stockIdQuantityAndSubtotal
-      return createRecordDetails(stockId, recordCreated.id, quantity, subtotal)
+      const { stockId, quantity, subtotal, historicalPrice } = stockIdQuantityAndSubtotal
+      return createRecordDetails(stockId, recordCreated.id, quantity, subtotal, historicalPrice)
     }
   )
   const allPromisesMovementsDetails = Promise.all(promiseArrayMovementsDetails)
@@ -246,23 +246,23 @@ const createDataOfRecord = async (
 ) => {
   return supplierId !== ''
     ? {
-        observation,
-        address,
-        letter,
-        recordNumber,
-        paidFor,
-        recordTypeId,
-        supplierId
-      }
+      observation,
+      address,
+      letter,
+      recordNumber,
+      paidFor,
+      recordTypeId,
+      supplierId
+    }
     : {
-        observation,
-        address,
-        letter,
-        recordNumber,
-        paidFor,
-        recordTypeId,
-        customerId
-      }
+      observation,
+      address,
+      letter,
+      recordNumber,
+      paidFor,
+      recordTypeId,
+      customerId
+    }
 }
 
 export {
