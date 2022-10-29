ccd1900af45520335c6cf40d7b36bd0778481496
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-29 00:46:40 -0300

    add: courses dashboard enpoint

---
+++ src/pages/api/dashboards/courses/index.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { coursesDashboards } from '../../../../backend/server/controllers/dashboards/coursesDashboards.controller'
+import { errorHandler } from '../../../../backend/server/controllers/errors/errorResponseHandler'
+
+export default async function coursesDashboard (
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req
+
+  switch (method) {
+    case 'GET':
+      try {
+        const courses = await coursesDashboards()
+        return res.status(200).send({ courses })
+      } catch (error) {
+        return errorHandler(res, error)
+      }
+
+    default:
+      return res.status(400).send('Invalid request')
+  }
+}
