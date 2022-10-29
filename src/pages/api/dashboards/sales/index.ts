449f10e5bf16a9afef61bf9e4dfb8240719f4df2
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-10-29 00:20:58 -0300

    add: sales endpoint

---
+++ src/pages/api/dashboards/sales/index.ts
@@ -0,0 +1,23 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getSales } from '../../../../backend/server/controllers/dashboards/dashboards.controller'
+import { errorHandler } from '../../../../backend/server/controllers/errors/errorResponseHandler'
+
+export default async function dashboards (
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { method } = req
+
+  switch (method) {
+    case 'GET':
+      try {
+        const sales = await getSales()
+        return res.status(200).send({ sales })
+      } catch (error) {
+        return errorHandler(res, error)
+      }
+
+    default:
+      return res.status(400).send('Invalid request')
+  }
+}
