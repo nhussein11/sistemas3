a397d8e5395a090e0b42e3640f07e61ce3daa920
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-11-01 22:07:46 -0300

    graph component

--- src/frontend/components/dashboard/Graph.tsx
+++ src/frontend/components/dashboard/Graph.tsx
@@ -1,191 +1,203 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Chart } from 'primereact/chart'
+import { useAxios } from '../../hooks/useAxios'
 
-const BarChartDemo = () => {
-  const [basicData] = useState({
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        label: 'My First dataset',
-        backgroundColor: '#42A5F5',
-        data: [65, 59, 80, 81, 56, 55, 40]
-      },
-      {
-        label: 'My Second dataset',
-        backgroundColor: '#FFA726',
-        data: [28, 48, 40, 19, 86, 27, 90]
-      }
-    ]
-  })
-
-  const getLightTheme = () => {
-    const basicOptions = {
-      maintainAspectRatio: false,
-      aspectRatio: 0.8,
-      plugins: {
-        legend: {
-          labels: {
-            color: '#495057'
-          }
-        }
-      },
-      scales: {
-        x: {
-          ticks: {
-            color: '#495057'
-          },
-          grid: {
-            color: '#ebedef'
-          }
-        },
-        y: {
-          ticks: {
-            color: '#495057'
-          },
-          grid: {
-            color: '#ebedef'
-          }
+const getLightTheme = () => {
+  const basicOptions = {
+    maintainAspectRatio: false,
+    aspectRatio: 0.8,
+    plugins: {
+      legend: {
+        labels: {
+          color: '#495057'
         }
       }
-    }
-
-    const horizontalOptions = {
-      indexAxis: 'y',
-      maintainAspectRatio: false,
-      aspectRatio: 0.8,
-      plugins: {
-        legend: {
-          labels: {
-            color: '#495057'
-          }
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: '#495057'
+        },
+        grid: {
+          color: '#ebedef'
         }
       },
-      scales: {
-        x: {
-          ticks: {
-            color: '#495057'
-          },
-          grid: {
-            color: '#ebedef'
-          }
+      y: {
+        ticks: {
+          color: '#495057'
         },
-        y: {
-          ticks: {
-            color: '#495057'
-          },
-          grid: {
-            color: '#ebedef'
-          }
+        grid: {
+          color: '#ebedef'
         }
       }
-    }
-
-    const stackedOptions = {
-      maintainAspectRatio: false,
-      aspectRatio: 0.8,
-      plugins: {
-        tooltips: {
-          mode: 'index',
-          intersect: false
-        },
-        legend: {
-          labels: {
-            color: '#495057'
-          }
-        }
-      },
-      scales: {
-        x: {
-          stacked: true,
-          ticks: {
-            color: '#495057'
-          },
-          grid: {
-            color: '#ebedef'
-          }
-        },
-        y: {
-          stacked: true,
-          ticks: {
-            color: '#495057'
-          },
-          grid: {
-            color: '#ebedef'
-          }
-        }
-      }
-    }
-
-    const multiAxisOptions = {
-      maintainAspectRatio: false,
-      aspectRatio: 0.8,
-      plugins: {
-        legend: {
-          labels: {
-            color: '#495057'
-          }
-        },
-        tooltips: {
-          mode: 'index',
-          intersect: true
-        }
-      },
-      scales: {
-        x: {
-          ticks: {
-            color: '#495057'
-          },
-          grid: {
-            color: '#ebedef'
-          }
-        },
-        y: {
-          type: 'linear',
-          display: true,
-          position: 'left',
-          ticks: {
-            min: 0,
-            max: 100,
-            color: '#495057'
-          },
-          grid: {
-            color: '#ebedef'
-          }
-        },
-        y1: {
-          type: 'linear',
-          display: true,
-          position: 'right',
-          grid: {
-            drawOnChartArea: false,
-            color: '#ebedef'
-          },
-          ticks: {
-            min: 0,
-            max: 100,
-            color: '#495057'
-          }
-        }
-      }
-    }
-
-    return {
-      basicOptions,
-      horizontalOptions,
-      stackedOptions,
-      multiAxisOptions
     }
   }
 
+  const horizontalOptions = {
+    indexAxis: 'y',
+    maintainAspectRatio: false,
+    aspectRatio: 0.8,
+    plugins: {
+      legend: {
+        labels: {
+          color: '#495057'
+        }
+      }
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: '#495057'
+        },
+        grid: {
+          color: '#ebedef'
+        }
+      },
+      y: {
+        ticks: {
+          color: '#495057'
+        },
+        grid: {
+          color: '#ebedef'
+        }
+      }
+    }
+  }
+
+  const stackedOptions = {
+    maintainAspectRatio: false,
+    aspectRatio: 0.8,
+    plugins: {
+      tooltips: {
+        mode: 'index',
+        intersect: false
+      },
+      legend: {
+        labels: {
+          color: '#495057'
+        }
+      }
+    },
+    scales: {
+      x: {
+        stacked: true,
+        ticks: {
+          color: '#495057'
+        },
+        grid: {
+          color: '#ebedef'
+        }
+      },
+      y: {
+        stacked: true,
+        ticks: {
+          color: '#495057'
+        },
+        grid: {
+          color: '#ebedef'
+        }
+      }
+    }
+  }
+
+  const multiAxisOptions = {
+    maintainAspectRatio: false,
+    aspectRatio: 0.8,
+    plugins: {
+      legend: {
+        labels: {
+          color: '#495057'
+        }
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: true
+      }
+    },
+    scales: {
+      x: {
+        ticks: {
+          color: '#495057'
+        },
+        grid: {
+          color: '#ebedef'
+        }
+      },
+      y: {
+        type: 'linear',
+        display: true,
+        position: 'left',
+        ticks: {
+          min: 0,
+          max: 100,
+          color: '#495057'
+        },
+        grid: {
+          color: '#ebedef'
+        }
+      },
+      y1: {
+        type: 'linear',
+        display: true,
+        position: 'right',
+        grid: {
+          drawOnChartArea: false,
+          color: '#ebedef'
+        },
+        ticks: {
+          min: 0,
+          max: 100,
+          color: '#495057'
+        }
+      }
+    }
+  }
+
+  return {
+    basicOptions,
+    horizontalOptions,
+    stackedOptions,
+    multiAxisOptions
+  }
+}
+
+const BarChartDemo = ({ label, color, endpoint }:any) => {
+  const { fetchedData, isLoading } = useAxios(endpoint)
   const { basicOptions } = getLightTheme()
-
+  console.log(fetchedData)
   return (
-        <div>
-            <div className="card">
-                <Chart type="bar" data={basicData} options={basicOptions} width={'400px'} height={'300px'}/>
-            </div>
-
+    <div>
+      {isLoading
+        ? (
+          <p>Loading...</p>
+          )
+        : (
+        <div className="card">
+          <Chart
+            type="bar"
+            data={{
+              labels: [
+                'October',
+                'November',
+                'December',
+                'January',
+                'February'
+              ],
+              datasets: [
+                {
+                  label,
+                  backgroundColor: color,
+                  data: []
+                }
+              ]
+            }}
+            options={basicOptions}
+            width={'400px'}
+            height={'300px'}
+          />
         </div>
+          )}
+    </div>
   )
 }
 export default BarChartDemo
