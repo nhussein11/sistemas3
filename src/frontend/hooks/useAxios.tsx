08f54ec4658ec25f80c2f0421ad6744cc1767124
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-11-01 22:07:34 -0300

    add:useaxios hook

---
+++ src/frontend/hooks/useAxios.tsx
@@ -0,0 +1,42 @@
+import { useState, useEffect } from 'react'
+import publicAxiosInstance from '../api/axios-api'
+
+interface IUseAxiosWithAbortResponse {
+  fetchedData: unknown
+  isLoading: boolean
+  error: Error | null
+}
+
+export const useAxios = (
+  endpoint: string
+): IUseAxiosWithAbortResponse => {
+  const [fetchedData, setFetchedData] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
+  console.log(fetchedData)
+  useEffect(() => {
+    const abortController = new AbortController()
+    const fetchData = async () => {
+      try {
+        const response = await publicAxiosInstance(endpoint, {
+          signal: abortController.signal
+        })
+        setIsLoading(false)
+        setFetchedData(response.data)
+      } catch (error) {
+        // @ts-ignore
+        if (error.name === 'AbortError') {
+          // @ts-ignore
+          setError(error)
+          setIsLoading(false)
+        }
+      }
+    }
+    fetchData()
+    return () => {
+      abortController.abort()
+    }
+  }, [endpoint])
+
+  return { fetchedData, isLoading, error }
+}
