da8b4d8abeada195f083190b23162b03bdea5b80
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-11-06 10:16:21 -0300

    add: datetime to courses model

--- src/db-cleanup-script.ts
+++ src/db-cleanup-script.ts
@@ -383,7 +383,7 @@ const createDefaultRecordDetails = async () => {
   const fulFilledResults = results.filter(
     (res) => res.status === 'fulfilled'
   ) as PromiseFulfilledResult<Omit<RecordDetails, 'id'>>[]
-  console.log(fulFilledResults)
+
   console.log('inserting default record details...')
   await prisma.recordDetails.createMany({
     data: fulFilledResults.map((res) => res.value)
@@ -423,6 +423,14 @@ const createDefaultStudents = async () => {
       birth: new Date(),
       phone: 321312321,
       email: 'nico@gmail.com'
+    },
+    {
+      name: 'Pablo',
+      surname: 'Perez',
+      identificationNumber: 4321345,
+      birth: new Date(),
+      phone: 38767890,
+      email: 'pablo@gmail.com'
     }
   ]
   const defaultStudentsWithCustomerId: Promise<Omit<Student, 'id'>>[] =
@@ -478,6 +486,12 @@ const createDefaultCourses = async () => {
       description: 'Curso de fundamentos de BDD',
       category: CategoryEnum.COURSE,
       price: 1500
+    },
+    {
+      name: 'Inscripcion - Curso de Arquitectura de Software',
+      description: 'Curso de fundamentos de Arquitectura de Software',
+      category: CategoryEnum.COURSE,
+      price: 3200
     }
   ]
 
@@ -510,22 +524,32 @@ const createDefaultCourses = async () => {
     {
       name: 'Curso de Front-End',
       hoursQuantity: 3,
-      productId: ''
+      productId: '',
+      datetime: new Date(Date.now())
     },
     {
       name: 'Curso de Back-End',
       hoursQuantity: 2,
-      productId: ''
+      productId: '',
+      datetime: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30)
     },
     {
       name: 'Curso de Testing',
       hoursQuantity: 4,
-      productId: ''
+      productId: '',
+      datetime: new Date(Date.now() + 1000 * 60 * 60 * 24 * 60)
     },
     {
       name: 'Curso de Bases de Datos',
       hoursQuantity: 1,
-      productId: ''
+      productId: '',
+      datetime: new Date(Date.now() + 1000 * 60 * 60 * 24 * 110)
+    },
+    {
+      name: 'Curso de Arquitectura de Software',
+      hoursQuantity: 1,
+      productId: '',
+      datetime: new Date(Date.now() + 1000 * 60 * 60 * 24 * 120)
     }
   ]
 
